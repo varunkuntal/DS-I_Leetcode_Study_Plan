4e65e106b02810158430f55bb220aa962b53b002
Varun Kuntal <varunkuntal@yahoo.in>
2022-06-06 23:40:41 +0530

    Day 2/14of DS 1 Study Plan - Day 78 of #100DaysOfCode

---
+++ Day_2/Merge_sorted_array.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/merge-sorted-array
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m - 1
+        p2 = n - 1
+        i = m + n - 1
+        
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[i] = nums1[p1]
+                i -= 1
+                p1 -= 1
+            else:
+                nums1[i] = nums2[p2]
+                i -= 1
+                p2 -= 1
+        
